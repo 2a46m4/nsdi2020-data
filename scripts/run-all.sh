74d5f1b080907655178172a8e9068e682e4f32cd
IJN Akashi AR <gcciscool@protonmail.com>
2025-02-06 00:05:10 -0800

    edit run-all

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -12,11 +12,11 @@ sudo ./00_setup_host.sh
 sudo ./run-system.sh -d $DIR
 
 ./run-boot-serial.sh   -d $DIR
-./run-boot-net.sh      -d $DIR
-./run-boot-parallel.sh -d $DIR -p 10
-./run-boot-parallel.sh -d $DIR -p 50
-./run-boot-parallel.sh -d $DIR -p 100
-./run-boot-kernel.sh   -d $DIR
+# ./run-boot-net.sh      -d $DIR
+# ./run-boot-parallel.sh -d $DIR -p 10
+# ./run-boot-parallel.sh -d $DIR -p 50
+# ./run-boot-parallel.sh -d $DIR -p 100
+# ./run-boot-kernel.sh   -d $DIR
 
-./run-mem.sh           -d $DIR
-./run-net.sh           -d $DIR
+# ./run-mem.sh           -d $DIR
+# ./run-net.sh           -d $DIR
